29439bad4cb87d4cb2903ea752bc6ce8f3090e75
xelmirage-pc <xelmirage@outlook.com>
2016-11-16 16:18:30 +0800

    a

--- wechat_image_cutter.py
+++ wechat_image_cutter.py
@@ -5,10 +5,9 @@ import sys
 
 def cut_for_moments(image_path_string, side_length):
     dir = os.path.dirname(image_path_string) + '/'
-    box_number = 3
     thumb_length = 257
     spacing = 9
-    total_thumb_length = thumb_length * box_number  + spacing * (box_number - 1)
+    total_thumb_length = thumb_length * side_length  + spacing * (side_length - 1)
     im = Image.open(image_path_string)
     '''
     -----
@@ -38,17 +37,19 @@ def cut_for_moments(image_path_string, side_length):
     box_2 = (cut_length_plus_space, 0, im_length, cut_length)
     box_3 = (0, cut_length_plus_space, cut_length, im_length)
     box_4 = (cut_length_plus_space, cut_length_plus_space, im_length, im_length)
-    for i in range(0, box_number):
-        for j in range(0, box_number):
+    for i in range(0, side_length):
+        for j in range(0, side_length):
             box = (int(j * cut_length_plus_space), int(i * cut_length_plus_space),
                    int(j * cut_length_plus_space + cut_length), int(i * cut_length_plus_space + cut_length))
             region = im.crop(box)
-            name = os.path.basename(image_path_string)[0:-4] + 'tile' + str(i * box_number + j)
+            name = os.path.basename(image_path_string)[0:-4] + 'tile' + str(i * side_length + j)
             fullpath = dir + name + '.jpg'
             region.save(fullpath, "JPEG")
-
+            print str(i * side_length + j)+",",
+            sys.stdout.flush()
+        print "\n"
 
 if __name__ == "__main__":
     file_path=sys.argv[1]
-    type=sys.argv[2]
-    cut_for_moments(file_path,2)
+    type=int(sys.argv[2])
+    cut_for_moments(file_path,type)
